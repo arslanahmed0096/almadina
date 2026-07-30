e4434b2049198f5674f9ab9d0312bf555f8ae3d3
arslanahmed096 <arslan06ahmed@gmail.com>
2026-07-30 03:07:37 -0700

    Upadte on superviouser

--- resources/src/views/app/pages/sales/index_sale.vue
+++ resources/src/views/app/pages/sales/index_sale.vue
@@ -1811,13 +1811,6 @@ export default {
     columns() {
       return [
         {
-          label: this.$t("Action"),
-          field: "actions",
-          tdClass: "text-left",
-          thClass: "text-left",
-          sortable: false
-        },
-        {
           label: this.$t("date"),
           field: "date",
           tdClass: "text-left",
@@ -1889,6 +1882,13 @@ export default {
         {
           label: this.$t("Documents"),
           field: "documents",
+          tdClass: "text-left",
+          thClass: "text-left",
+          sortable: false
+        },
+        {
+          label: this.$t("Action"),
+          field: "actions",
           tdClass: "text-left",
           thClass: "text-left",
           sortable: false
